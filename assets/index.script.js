870ec11342045a79899e816625c303c625f16340
AnthonyLuiz0 <flipyytbrorg1@gmail.com>
2025-06-04 10:00:19 -0300

    Add files via upload

---
+++ assets/index.script.js
@@ -0,0 +1,36 @@
+$(function(){
+    $('.iframe-editor').hide()
+    $(`.iframe-editor[data-editor="${$('.tab-selected').data('editor')}"]`).show()
+    
+    $('.tab-item').click(function(){
+        $('.tab-item').removeClass('tab-selected')
+        $(this).addClass('tab-selected')
+        $('.iframe-editor').hide()
+        $(`.iframe-editor[data-editor="${$(this).data('editor')}"]`).show()
+    })
+    $('.iframe-editor div').on('input', function(){
+        const html = $('.iframe-editor[data-editor="html"] div').html()
+        const css = $('.iframe-editor[data-editor="css"] div').html()
+        const js = $('.iframe-editor[data-editor="js"] div').html()
+
+        $('.iframe-editor[data-editor="html"] div').html(html.replace(/(<[^>]+>)/g, '<span style="color:#333 !important">$1</span>'));
+
+        const content = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"><\/script>
+            <style>${css}</style>
+        </head>
+        <body>
+            ${html}
+            <script>${js}</script>
+        </body>
+        </html>`
+        
+        $('.output iframe').attr('srcdoc', content)
+
+    })
+})
